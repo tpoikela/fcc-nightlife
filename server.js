2cf8f9466f420942d00627ed0ae32ab6a36fca9e
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-01-05 14:27:33 +0100

    Added /public path.

--- server.js
+++ server.js
@@ -3,20 +3,29 @@
  */
 'use strict';
 
+var $DEBUG = 0;
+
+// When deployed to heroku, don't use .env
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').load();
+    $DEBUG = process.env.DEBUG || 0;
+    if ($DEBUG) console.log("Loaded .env file OK. Node env: " 
+        + process.env.NODE_ENV);
+}
+
+// Load required modules
 var express = require('express');
-var routes = require('./app/routes/index.js');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var session = require('express-session');
 var bodyParser = require('body-parser');
 
+// Sources for this app
+var routes = require('./app/routes/index.js');
+
 var app = express();
 app.set("view engine", "pug");
 
-// When deployed to heroku, don't use .env
-if (process.env.NODE_ENV !== "production") {
-    require('dotenv').load();
-}
 
 require('./app/config/passport')(passport);
 
@@ -26,6 +35,8 @@ console.log("APP url: " + app.url);
 mongoose.connect(process.env.MONGO_URI);
 mongoose.Promise = global.Promise;
 
+// Initialize resource paths for the server
+app.use('/build', express.static(process.cwd() + '/build'));
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use('/common', express.static(process.cwd() + '/app/common'));
