fe961a4fbebabacc80c9305b2b7d49aabdb19988
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-01-08 23:39:46 +0100

    Removed unused test.

--- tests/usersTest.js
+++ tests/usersTest.js
@@ -3,7 +3,6 @@ const chai = require("chai");
 const expect = chai.expect;
 
 const User = require("../app/models/users.js");
-const Poll = require("../app/models/polls.js");
 
 describe('User Schema', function() {
 
@@ -16,15 +15,6 @@ describe('User Schema', function() {
         error = user.validateSync();
         expect(typeof error).to.equal("undefined");
 
-    });
-
-    it('contains a list of poll IDs', function() {
-        var poll = new Poll();
-        poll.save(function(err) {
-            user.polls.push(poll._id);
-            var error = user.validateSync();
-            expect(error.errors).to.not.have.property("polls");
-        });
     });
 
 
