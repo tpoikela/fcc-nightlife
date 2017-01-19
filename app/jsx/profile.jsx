561754a2a2d9b4b196cf9c87897f6de8f0010c4b
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-01-20 00:09:43 +0100

    Split the app.jsx into top.jsx and added a conditional rendering of top-level components.

---
+++ app/jsx/profile.jsx
@@ -0,0 +1,29 @@
+
+'use strict';
+
+var ajax = require('../common/ajax-functions.js');
+
+/** This component is used at the profile page of a user.*/
+class ProfileTop extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    componenDidMount() {
+        //this.getUser
+
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Placeholder for actual component.</p>
+            </div>
+        );
+
+    }
+
+}
+
+module.exports = ProfileTop;
