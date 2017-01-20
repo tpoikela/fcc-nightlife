279b21a714f3d82e9b262d756ab0f591b5f77dad
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-01-20 13:57:29 +0100

    Implemented componentDidMount which requests userInfo.

--- app/jsx/profile.jsx
+++ app/jsx/profile.jsx
@@ -1,24 +1,46 @@
 
 'use strict';
 
-var ajax = require('../common/ajax-functions.js');
+const appUrl = window.location.origin;
+const ajax = require('../common/ajax-functions.js');
+const UserController = require('../controllers/userController.client');
 
 /** This component is used at the profile page of a user.*/
 class ProfileTop extends React.Component {
 
     constructor(props) {
         super(props);
+        this.userCtrl = new UserController(appUrl);
+
+        this.state = {
+            username: '',
+            userID: '',
+        };
     }
 
-    componenDidMount() {
-        //this.getUser
+    componentDidMount() {
+        this.userCtrl.amIAuthorized( (err, data) => {
+            if (err) {
+                this.setState({error: 'Error occurred, Please refresh.'});
+            }
+            else {
+                this.setState({
+                    username: data.username,
+                    userID: data.userID,
+                });
+            }
 
+        });
     }
 
     render() {
+        var username = this.state.username;
+        var userID = this.state.userID;
         return (
             <div>
                 <p>Placeholder for actual component.</p>
+                <p>Welcome {username}</p>
+                <p>User ID: {userID}</p>
             </div>
         );
 
