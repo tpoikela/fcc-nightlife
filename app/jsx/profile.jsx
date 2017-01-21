df7c29a4a605eded289fdf9289cc8ccb42e431b8
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-01-21 23:44:30 +0100

    Added ProfileVenueItem.

--- app/jsx/profile.jsx
+++ app/jsx/profile.jsx
@@ -5,12 +5,40 @@ const appUrl = window.location.origin;
 const ajax = require('../common/ajax-functions.js');
 const UserController = require('../controllers/userController.client');
 
+/** Renders one venue item for the profile page with all controls.*/
+class ProfileVenueItem extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.onRemoveClick = this.onRemoveClick.bind(this);
+    }
+
+    onRemoveClick() {
+        this.props.onRemoveClick(this.props.venue.appID);
+    }
+
+    render() {
+        var venue = this.props.venue;
+        var venueStr = JSON.stringify(venue);
+
+        return (
+            <li>
+                {venueStr}
+                <button onClick={this.onRemoveClick}>Remove</button>
+            </li>
+        );
+    }
+
+}
+
 /** This component is used at the profile page of a user.*/
 class ProfileTop extends React.Component {
 
     constructor(props) {
         super(props);
         this.userCtrl = new UserController(appUrl);
+
+        this.onRemoveClick = this.onRemoveClick.bind(this);
 
         this.state = {
             username: null,
@@ -21,6 +49,11 @@ class ProfileTop extends React.Component {
 
     log(msg) {
         console.log("ProfileTop Log: " + msg);
+    }
+
+    // TODO: Remove a venue where user is going to
+    onRemoveClick(appID) {
+        console.log("onRemoveClick with appID: " + appID);
 
     }
 
@@ -42,12 +75,14 @@ class ProfileTop extends React.Component {
         });
     }
 
+    /** Get user info from the server.*/
     getUserInfo() {
         var username = this.state.username;
         this.userCtrl.getUserProfileData(username, (err, data) => {
             if (err) this.setState({error: "An error occurred."});
             else {
                 this.log("ProfileTop got data: " + JSON.stringify(data));
+                this.setState({venues: data.venues});
             }
         });
     }
@@ -59,11 +94,25 @@ class ProfileTop extends React.Component {
     render() {
         var username = this.state.username;
         var userID = this.state.userID;
+        var venues = this.state.venues;
+
+        // Generate the list of venues here
+        var venueHtml = null;
+        if (venues) {
+            venueHtml = venues.map( (item, index) => {
+                return (<ProfileVenueItem key={index} venue={item} 
+                    onRemoveClick={this.onRemoveClick} />);
+            });
+        }
+
         return (
             <div>
-                <p>Placeholder for actual component.</p>
                 <p>Welcome {username}</p>
                 <p>User ID: {userID}</p>
+                <p>You're currently going to the following places:</p>
+                <ul>
+                    {venueHtml}
+                </ul>
             </div>
         );
 
