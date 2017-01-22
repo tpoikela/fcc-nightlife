08072c80dc4a69ce255313641dd2edc039fcfbcf
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-01-22 15:07:45 +0100

    Added gulp-sass. Added sass task to compile scss into  css.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
 var babelify = require('babelify');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+
+var spawnSync = require('child_process').spawnSync;
 
 var jsxDir = "./app/jsx";
 
@@ -15,9 +18,24 @@ gulp.task('build', function () {
         .pipe(gulp.dest('build'));
 });
 
+gulp.task('sass', function() {
+    console.log("Watching folder ./scss for changes.");
+	return gulp.src('./sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./build'));
+
+});
+
 gulp.task('watch', ['build'], function () {
     //gulp.watch(jsxDir + '/*.jsx', ['build']);
     gulp.watch('./app/**/*.*', ['build']);
+    gulp.watch('./scss/*.*', ['sass']);
+});
+
+gulp.task('tags', function() {
+    console.log("Building ctags for the project.");
+    spawnSync('ctags', ['-R', 'app/', 'pug/', 'scss/']);
 });
 
 gulp.task('default', ['watch']);
+
