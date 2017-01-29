08993f539e9e35a4928c9a2449d40452688e9ffc
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-01-29 10:10:00 +0100

    Added test for checking addding of venues. Still failing.

--- tests/usersTest.js
+++ tests/usersTest.js
@@ -1,19 +1,76 @@
 
-const chai = require("chai");
+const chai = require('chai');
+const sinon = require('sinon');
+const mongoose = require('mongoose');
+const User = require('../app/models/users.js');
+
 const expect = chai.expect;
 
-const User = require("../app/models/users.js");
+describe('User Model', () => {
 
-describe('User Schema', function() {
+    var userUpdate = null;
 
-    it('should have a valid username', function() {
+    // Stub out the update which interacts with database
+    beforeEach(() => {
+        userUpdate = sinon.stub(User, 'update');
+    });
+
+    afterEach(() => {
+        userUpdate.restore();
+    });
+
+
+    it('should have a valid username', () => {
         var user = new User();
         var error = user.validateSync();
         expect(error.errors.username).to.exist;
 
-        user.username = "Tuomas";
+        user.username = 'Tuomas';
         error = user.validateSync();
-        expect(typeof error).to.equal("undefined");
+        expect(typeof error.errors.username).to.equal('undefined');
+
+
+    });
+
+    it('should have login information', () => {
+        var user = new User();
+        var error = user.validateSync();
+        expect(typeof error.errors.local).to.exist;
+        console.log(JSON.stringify(error));
+
+        var localInv = {password: 'xxx'};
+        user.local = localInv;
+        error = user.validateSync();
+        console.log(JSON.stringify(error));
+        expect(typeof error.errors.local.password).to.equal('undefined');
+        expect(typeof error.errors.local.username).to.exist;
+
+        var local = {username: 'xxx', password: 'xxx'};
+        user.local = local;
+        error = user.validateSync();
+        expect(typeof error.errors.local).to.equal('undefined');
+    });
+
+    it('can have venues added to it', (done) => {
+        var user = new User();
+        user._id = mongoose.Types.ObjectId();
+        user.username = 'xxx';
+        user.local = {username: 'xxx', password: 'xxx'};
+
+        var venueID = mongoose.Types.ObjectId();
+        userUpdate.yields(null);
+
+        var setQuery = {$set: {venues: [venueID]}};
+
+        var cb = (err) => {
+            expect(err).to.be.null;
+            sinon.assert.calledOnce(userUpdate);
+            sinon.assert.calledWith(userUpdate, {_id: venueID}, setQuery, {},
+                sinon.match.any);
+            done();
+        };
+
+        user.addVenue(venueID, cb);
 
     });
 
